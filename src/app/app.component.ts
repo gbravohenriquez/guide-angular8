466662f0ca76ff257a365c03e8ddc9689c25063c
Guillermo Bravo <gbravo@seiza-ti.cl>
2019-10-14 14:14:11 -0300

    clase 41

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,7 +18,7 @@ export class AppComponent {
 
   onToggleDetails() {
     this.showSecret = !this.showSecret;
-    this.log.push(this.log.length + 1);
+    this.log.push(new Date());
     console.log(this.log);
   }
 }
