3e1ef93716b7ba5034df51900946a51468a2fe13
Guillermo Bravo <gbravo@seiza-ti.cl>
2019-10-14 15:16:32 -0300

    Clase 48, creando estructura base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,22 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // styleUrls: ['./app.component.css']
-  /*styles: [`
-  h3{
-    color: dodgerblue;
-  }`]*/
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  username = '';
-
-  showSecret = false;
-  log = [];
-
-  onToggleDetails() {
-    this.showSecret = !this.showSecret;
-    this.log.push(new Date());
-    console.log(this.log);
-  }
 }
