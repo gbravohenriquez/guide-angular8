602f5d318616427da19dfcf5c22f1853f6544b41
Guillermo Bravo <gbravo@seiza-ti.cl>
2019-10-15 10:10:15 -0300

    fixed

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Ingredient} from '../shared/ingredient.model';
+import {Recipe} from '../recipes/recipe.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -7,7 +8,7 @@ import {Ingredient} from '../shared/ingredient.model';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients: Ingredient = [
+  ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 1)
   ];
