8c7b0ddf9ab42b6e95f40e3efa177adaac9a8469
Guillermo Bravo <gbravo@seiza-ti.cl>
2019-10-16 12:39:41 -0300

    clase 90, ngFor ngIf repaso

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,16 +5,9 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
 export class AppComponent {
-  oddNumbers: number[] = [];
-  evenNumbers: number[] = [];
-
-  onIntervalFired(firedNumber: number) {
-    if (firedNumber % 2 === 0) {
-      this.evenNumbers.push(firedNumber);
-    } else {
-      this.oddNumbers.push(firedNumber);
-    }
-  }
+  //numbers = [1, 2, 3, 4, 5];
+  oddNumbers = [1, 3, 5];
+  evenNumbers = [2, 4];
+  onlyOdd = false;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-
 import { AppComponent } from './app.component';
-import { OddComponent } from './odd/odd.component';
-import { GameControlComponent } from './game-control/game-control.component';
-import { EvenComponent } from './even/even.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    OddComponent,
-    GameControlComponent,
-    EvenComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-
