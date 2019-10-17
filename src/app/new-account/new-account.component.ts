91926b6d4c7aa0c8ceab80adab0b411f1e109e9c
Guillermo Bravo <gbravo@seiza-ti.cl>
2019-10-17 10:33:04 -0300

    clase 106 y 107, Creating a Data Service

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import {LoggingService} from '../logging.service';
+import {AccountsService} from '../accounts.service';
 
 @Component({
   selector: 'app-new-account',
@@ -8,16 +9,12 @@ import {LoggingService} from '../logging.service';
   providers: [LoggingService]
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
 
-  constructor(private loggingService: LoggingService) {}
+  constructor(private loggingService: LoggingService,
+              private  accountService: AccountsService) {}
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
-
+    this.accountService.addAccount(accountName, accountStatus);
     this.loggingService.logTStatusChange(accountStatus);
   }
 }
