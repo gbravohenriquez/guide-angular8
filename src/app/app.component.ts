c13e6833bbc5bf3335008e7aa41e795160a6f337
Guillermo Bravo <gbravo@seiza-ti.cl>
2019-10-17 18:08:47 -0300

    clase 171, custom Observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
-
-import { UserService } from './users.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [UserService]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-
 
 import { AppComponent } from './app.component';
-import { ActiveUsersComponent } from './active-users/active-users.component';
-import { InactiveUsersComponent } from './inactive-users/inactive-users.component';
-import { CounterService } from './counter.service';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ActiveUsersComponent,
-    InactiveUsersComponent
+    HomeComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    AppRoutingModule
   ],
-  providers: [CounterService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
