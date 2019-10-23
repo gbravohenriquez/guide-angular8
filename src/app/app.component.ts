b3594fc1865f4e2119f15d1590afe160b2c1b502
Guillermo Bravo <gbravo@seiza-ti.cl>
2019-10-23 11:23:53 -0300

    clase 185 y 186, submittingand using the form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,16 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import {UserService} from './user.service';
-import {Subscription} from 'rxjs';
+import {Component, ElementRef} from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, OnDestroy {
-  userActivated = false;
-  private activatedSub: Subscription;
-
-  constructor(private userService: UserService) {}
-
-  ngOnInit() {
-    this.activatedSub = this.userService.activatedEmitter.subscribe(didActivate => {
-      this.userActivated = didActivate;
-    });
+export class AppComponent {
+  suggestUserName() {
+    const suggestedName = 'Superuser';
   }
-
-  ngOnDestroy(): void {
-    this.activatedSub.unsubscribe();
+  onSubmit(form: NgForm) {
+    console.log(form);
   }
 }
