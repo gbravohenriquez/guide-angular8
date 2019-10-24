e951aa5c754f1c05f23deb5971e900e9f235d126
Guillermo Bravo <gbravo@seiza-ti.cl>
2019-10-23 23:05:11 -0300

    clase 216, TD: Adding Validation to the Form

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,6 +1,7 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Ingredient} from '../../shared/ingredient.model';
 import {ShoppingListService} from '../shopping-list.service';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -8,24 +9,19 @@ import {ShoppingListService} from '../shopping-list.service';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
-@ViewChild('nameInput') nameInputRef: ElementRef;
-@ViewChild('amountInput') amountInputRef: ElementRef;
 
   constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit() {
   }
 
-  addIngredient() {
-    const ingName = this.nameInputRef.nativeElement.value;
-    const ingAmount = this.amountInputRef.nativeElement.value;
-    //this.ingredienteEvento.emit(new Ingredient(ingName, ingAmount));
-    this.shoppingListService.addIngredient(new Ingredient(ingName, ingAmount));
+  onAddItem(form: NgForm) {
+    console.log(form);
+    const value = form.value;
+    const newIngredient = new Ingredient(value.name, value.amount);
+    this.shoppingListService.addIngredient(newIngredient);
   }
-  clearList() {
-    //this.clearEvento.emit();
-    this.shoppingListService.clearList();
-  }
+
 
 
 }
